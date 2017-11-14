2017b730552afa907fc2b3c1dc232af9d4c17f80
Jon Vivian <4leite@gmail.com>
2017-11-14 22:08:45 +1300

    add global_admin role

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 site1 = Site.find_or_create_by(name: "NZ Wine")
 site2 = Site.find_or_create_by(name: "A Winery")
 
-user1 = User.create(site: site1, roles: "dev", email: "jon@4pi.nz", password: "changem3!", password_confirmation: "changem3!")
+user1 = User.create(site: site1, roles: "dev global_admin", email: "jon@4pi.nz", password: "changem3!", password_confirmation: "changem3!")
 
 user2 = User.create(site: site1, roles: "global_admin", email: "jaketipler@gmail.com", password: "changem3!", password_confirmation: "changem3!")
 
