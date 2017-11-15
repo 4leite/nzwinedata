42cbadb3f17ac9213ae9fc22fea6e803cd0a8e5b
Jon Vivian <4leite@gmail.com>
2017-11-16 02:50:37 +1300

    change ability definitions to be clearer for navbar add sales menu move menu logic to main menu view

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,9 +6,9 @@ class Ability
     
     @user = user || User.new
 
-    if @user.roles.size === 0 
+    if @user.roles.blank?
       can :index, :home 
-    else  
+    else
       @user.roles.each { |role| send(role.to_sym) }
     end
   end
@@ -19,18 +19,22 @@ class Ability
   end
 
   def miner
+    nav_miner
     can :show, Site, id: @user.site_id
     can :manage, DailySale, site_id: @user.site_id
     guest
   end
 
   def global_admin
-    admin
+    nav_miner
+    nav_admin
     can :manage, :all
+    guest
   end
 
   def site_admin
-    admin
+    nav_miner
+    nav_admin
     can [:show, :update], Site, id: @user.site_id
     can :manage, User, site_id: @user.site_id
     can :manage, DailySale, site_id: @user.site_id
@@ -43,8 +47,12 @@ class Ability
 
   # abstract ability classes
 
-  def admin
-    can :view, :administrate
+  private
+  def nav_miner
+    can :navbar, :administrate
+  end
+  def nav_admin
+    can :navbar, :administrate
   end
 
 end
