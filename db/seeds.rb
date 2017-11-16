eb8385ab45e39aaae0d93179b92fd6018992c7ef
Jon Vivian <4leite@gmail.com>
2017-11-16 23:57:34 +1300

    remove passwords from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+default_pwd = ENV['RAILS_DEFAULT_PWD']
+
 site1 = Site.find_or_create_by(name: "NZ Wine")
 site2 = Site.find_or_create_by(name: "A Winery")
 
-user1 = User.create(site: site1, roles: [ "dev", "global_admin" ], email: "jon@4pi.nz", password: "changem3!", password_confirmation: "changem3!")
+user1 = User.create(site: site1, roles: [ "dev", "global_admin" ], email: "jon@4pi.nz", password: default_pwd, password_confirmation: default_pwd)
 
-user2 = User.create(site: site1, roles: "global_admin", email: "jaketipler@gmail.com", password: "changem3!", password_confirmation: "changem3!")
+user2 = User.create(site: site1, roles: "global_admin", email: "jaketipler@gmail.com", password: default_pwd, password_confirmation: default_pwd)
 
-user3 = User.create(site: site2, roles: "site_admin", email: "manager@4pi.nz", password: "changem3!", password_confirmation: "changem3!")
+user3 = User.create(site: site2, roles: "site_admin", email: "manager@4pi.nz", password: default_pwd, password_confirmation: default_pwd)
 
-user4 = User.create(site: site2, roles: "miner", email: "miner@4pi.nz", password: "changem3!", password_confirmation: "changem3!")
+user4 = User.create(site: site2, roles: "miner", email: "miner@4pi.nz", password: default_pwd, password_confirmation: default_pwd)
 
 
 
